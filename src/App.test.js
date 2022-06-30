446b49bd65e8e3c2e91d159b162caeb52ca80976
matfij <mateusz.michal.fijak@gmail.com>
2022-06-30 22:55:21 +0200

    reinitialize project - next

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import App from "./App";
-
-test("renders learn react link", () => {
-  render(<App />, { wrapper: MemoryRouter });
-  const linkElement = screen.getByText(/gallery/i);
-  expect(linkElement).toBeInTheDocument();
-});
