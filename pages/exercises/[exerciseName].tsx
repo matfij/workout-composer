0f5a115e9dc6c12ffa99bdc35a110725b4453101
matfij <mateusz.michal.fijak@gmail.com>
2022-07-05 20:49:06 +0200

    add ssg for dynamic routes

--- pages/exercises/[exerciseName].tsx
+++ pages/exercises/[exerciseName].tsx
@@ -1,20 +1,38 @@
-import { NextPage } from "next";
-import { useRouter } from "next/router";
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { DUMMY_EXERCISES } from ".";
 import ExerciseDetails from "../../components/exercise/ExerciseDetails";
+import { Exercise } from "../../components/exercise/ExerciseItem";
 
-const ExerciseDetailPage: NextPage = () => {
-  const router = useRouter();
+type Props = {
+  exercise: Exercise;
+}
 
-  const exerciseName = router.query.exerciseName;
+const ExerciseDetailPage: NextPage<Props> = (props: Props) => {
+
+  return (
+    <ExerciseDetails {...props.exercise} />
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pathsData = DUMMY_EXERCISES.map((exercise) => { return { params: { exerciseName: exercise.name } } });
+
+  return {
+    paths: pathsData,
+    fallback: false,  // is the paths prop (not) exhaustive?
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const exerciseName = context.params?.exerciseName;
 
   const exercise =
     DUMMY_EXERCISES.find((exercise) => exercise.name === exerciseName) ||
     DUMMY_EXERCISES[0];
 
-  return (
-    <ExerciseDetails {...exercise} />
-  );
+  return {
+    props: { exercise: exercise }
+  }
 };
 
 export default ExerciseDetailPage;
