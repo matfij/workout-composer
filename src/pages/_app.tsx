3b7b23784e48391e393952d0e89a01c4a7c19b2e
matfij <mateusz.michal.fijak@gmail.com>
2022-07-25 15:50:17 +0200

    add copy and load board data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
-import '../styles/globals.css'
-import '../styles/common.css'
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css';
+import '../styles/common.css';
+import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
+import { BoardDataProvider } from '../context/BoardContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BoardDataProvider>
+      <Component {...pageProps} />
+    </BoardDataProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,50 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import ExerciseAdd from '../components/exercise-add';
 import ExerciseBoard from '../components/exercise-board';
-import { BoardDataProvider } from '../context/BoardContext';
+import { BoardData, useBoardData, useSetBoardDataContext } from '../context/BoardContext';
+import { v4 as uuidv4 } from 'uuid';
 
-const Home: NextPage = () => {
+function Home() {
+  const boardData = useBoardData();
+  const updateBoardData = useSetBoardDataContext();
   const [winReady, setwinReady] = useState(false);
   const [displayExerciseAdd, setDisplayExerciseAdd] = useState(false);
+  const router = useRouter();
 
   useEffect(() => {
     setwinReady(true);
-  }, []);
+
+    const { bdata } = router.query;
+    try {
+      const loadedBoardData = JSON.parse(decodeURIComponent(bdata as string)) as BoardData;
+      const savedBoardData: BoardData = {
+        days: loadedBoardData.days.map((day) => ({
+          day: day.day,
+          exercises: day.exercises.map((exercise) => ({ ...exercise, id: uuidv4() })),
+        })),
+        standby: loadedBoardData.standby.map((exercise) => ({ ...exercise, id: uuidv4() })),
+      };
+      updateBoardData(savedBoardData);
+    } catch (ex) {}
+  }, [router.query]);
 
   const toggleExerciseAdd = () => {
     setDisplayExerciseAdd(!displayExerciseAdd);
+  };
+
+  const copyLink = () => {
+    const minBoardData: BoardData = {
+      days: boardData.days.map((day) => ({
+        day: day.day,
+        exercises: day.exercises.map((exercise) => ({ ...exercise, id: '' })),
+      })),
+      standby: boardData.standby.map((exercise) => ({ ...exercise, id: '' })),
+    };
+    navigator.clipboard.writeText(
+      `${window.location.href}?bdata=${encodeURIComponent(JSON.stringify(minBoardData))}`
+    );
   };
 
   return (
@@ -29,21 +59,22 @@ const Home: NextPage = () => {
       </Head>
 
       <h1 className="w-full text-center p-3 sm:p-6 text-xl sm:text-3xl text-teal-600">Workout Composer</h1>
-      <BoardDataProvider>
-        <div className="flex flex-col">
-          {winReady ? <ExerciseBoard /> : null}
+      <div className="flex flex-col">
+        {winReady ? <ExerciseBoard /> : null}
 
-          <div className="w-full max-w-sm m-auto pt-6">
-            <button onClick={toggleExerciseAdd} className="btnPrimary btnFloat sm:w-72">
-              New
-            </button>
-          </div>
+        <div className="w-full max-w-sm m-auto pt-6">
+          <button onClick={toggleExerciseAdd} className="btnPrimary btnFloat sm:w-72">
+            New
+          </button>
+          <button onClick={copyLink} className="btnPrimary btnFloat sm:w-72 mb-20 sm:mb-16">
+            Copy link
+          </button>
         </div>
+      </div>
 
-        {displayExerciseAdd && <ExerciseAdd onCancel={toggleExerciseAdd} />}
-      </BoardDataProvider>
+      {displayExerciseAdd && <ExerciseAdd onCancel={toggleExerciseAdd} />}
     </>
   );
-};
+}
 
 export default Home;
