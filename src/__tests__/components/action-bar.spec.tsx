f5a690931aa9ffb0981bb27780a9149cde918da9
domgan <domiran35@gmail.com>
2023-01-09 14:55:32 +0100

    style:  improve action-bar tests file

--- src/__tests__/components/action-bar.spec.tsx
+++ src/__tests__/components/action-bar.spec.tsx
@@ -4,18 +4,18 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import ActionBar from '../../features/workout-composer/components/action-bar.component';
 import { useRouter } from 'next/router'
 
-jest.mock('next/router', () => ({useRouter: jest.fn()}))
+jest.mock('next/router', () => ({ useRouter: jest.fn() }));
 
 describe('Home button', () => {
   it('Redirects to home directory', () => {
-    const mockRouter = {push: jest.fn()};
-    (useRouter as jest.Mock).mockReturnValue(mockRouter)
+    const mockRouter = { push: jest.fn() };
+    (useRouter as jest.Mock).mockReturnValue(mockRouter);
 
-    render(<ActionBar/>);
+    render(<ActionBar />);
 
     const homeButton = screen.getByTestId('home-button');
-    fireEvent.click(homeButton)
+    fireEvent.click(homeButton);
 
-    expect(mockRouter.push).toHaveBeenCalledWith('/')
+    expect(mockRouter.push).toHaveBeenCalledWith('/');
   });
 });
