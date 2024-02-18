4ced52a03ecf812c16c410265538784d707cdda8
matfij <mateusz.michal.fijak@gmail.com>
2024-02-18 14:29:44 +0100

    add auto input  switching

--- src/features/darts-scoreboard/components/update-scores-form.component.tsx
+++ src/features/darts-scoreboard/components/update-scores-form.component.tsx
@@ -8,6 +8,7 @@ import {
 import ToastService from '../../../common/services/toast-service';
 import { ALLOWED_SCORES } from '../definitions/constants';
 import UtilService from '../../../common/services/utils-service';
+import { ChangeEvent } from 'react';
 
 type Props = {
   user: DartsUser;
@@ -20,42 +21,55 @@ export default function UpdateScoresForm(props: Props) {
   const updateBoard = useSetDartsScoreboardContext();
 
   const validateScore = (score: number): boolean => {
-    console.log(ALLOWED_SCORES.includes(+score));
     return ALLOWED_SCORES.includes(+score);
   };
 
+  const checkInputSwitch = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length < 2) {
+      return;
+    }
+    (event.target.nextElementSibling as HTMLInputElement)?.focus();
+  };
+
   const updateScores = (data: UpdateScoresFields) => {
+    const scores = calculateScores(data);
+    if (scores === 0) {
+      notifyWinner();
+    }
+    updateUser(scores, [data.throw1, data.throw2, data.throw3]);
+    props.onCancel();
+  };
+
+  const calculateScores = (data: UpdateScoresFields) => {
+    let scores = props.user.scores;
     data.throw1 = +data.throw1 ?? 0;
     data.throw2 = +data.throw2 ?? 0;
     data.throw3 = +data.throw3 ?? 0;
+    if (scores - data.throw1 >= 0) {
+      scores -= data.throw1;
+    }
+    if (scores - data.throw2 >= 0) {
+      scores -= data.throw2;
+    }
+    if (scores - data.throw3 >= 0) {
+      scores -= data.throw3;
+    }
+    return scores;
+  };
 
-    let newScores = props.user.scores;
-    if (newScores - data.throw1 >= 0) {
-      newScores -= data.throw1;
-    }
-    if (newScores - data.throw2 >= 0) {
-      newScores -= data.throw2;
-    }
-    if (newScores - data.throw3 >= 0) {
-      newScores -= data.throw3;
-    }
-    if (newScores === 0) {
-      UtilService.playSound('/sounds/dart-win-sound.mp3');
-      ToastService.showInfo(`${props.user.name} has won!`);
-    }
+  const notifyWinner = () => {
+    UtilService.playSound('/sounds/dart-win-sound.mp3');
+    ToastService.showInfo(`${props.user.name} has won!`);
+  };
+
+  const updateUser = (newScores: number, throws: number[]) => {
+    const updatedUser = { ...props.user };
+    updatedUser.scores = newScores;
+    updatedUser.throws = [...updatedUser.throws, ...throws];
+    const updatedUsers = board.users.map((user) => (user.name === updatedUser.name ? updatedUser : user));
     updateBoard({
-      users: board.users.map((user) => {
-        if (user.name !== props.user.name) {
-          return user;
-        }
-        return {
-          ...user,
-          scores: newScores,
-          throws: [...user.throws, data.throw1, data.throw2, data.throw3],
-        };
-      }),
+      users: updatedUsers,
     });
-    props.onCancel();
   };
 
   return (
@@ -69,21 +83,30 @@ export default function UpdateScoresForm(props: Props) {
             <fieldset className="mb-4 flex gap-1">
               <input
                 {...register('throw1', { validate: (x) => validateScore(x) })}
+                onChange={(e) => checkInputSwitch(e)}
                 className={style.formInput}
                 id="throw1"
                 type="number"
+                min={0}
+                max={60}
               />
               <input
                 {...register('throw2', { validate: (x) => validateScore(x) })}
+                onChange={(e) => checkInputSwitch(e)}
                 className={style.formInput}
                 id="throw2"
                 type="number"
+                min={0}
+                max={60}
               />
               <input
                 {...register('throw3', { validate: (x) => validateScore(x) })}
+                onChange={(e) => checkInputSwitch(e)}
                 className={style.formInput}
                 id="throw3"
                 type="number"
+                min={0}
+                max={60}
               />
             </fieldset>
             <div className="flex items-center pt-4 space-x-2">
