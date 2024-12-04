f64d7ae9bb14b960b8020551447713672f88c8de
matfij <mateusz.michal.fijak@gmail.com>
2024-12-04 20:43:52 +0100

    setup dnd context

--- next15/src/app/workout-composer/page.tsx
+++ next15/src/app/workout-composer/page.tsx
@@ -5,11 +5,17 @@ import { useState } from 'react';
 import { MenuComponent } from './components/menu-component';
 import { TaskFormComponent } from './components/task-form-component';
 import { useWorkoutStore } from './workout-store';
+import { DragDropContext, Droppable, DropResult } from '@hello-pangea/dnd';
+import { TaskCardComponent } from './components/task-card-component';
 
 export default function WorkoutComposerPage() {
     const { freeTasks } = useWorkoutStore();
     const [showTaskForm, setShowTaskForm] = useState(false);
     const [showDayForm, setShowDayForm] = useState(false);
+
+    const onMoveTask = (e: DropResult) => {
+        console.log(e);
+    };
 
     return (
         <>
@@ -17,7 +23,17 @@ export default function WorkoutComposerPage() {
                 <h1 className="title" style={{ marginBottom: '0.5rem' }}>
                     Workout Composer
                 </h1>
-                <pre>{JSON.stringify(freeTasks)}</pre>
+                <DragDropContext onDragEnd={onMoveTask}>
+                    <Droppable droppableId="freeTasks">
+                        {(dropProvider) => (
+                            <div {...dropProvider.droppableProps} ref={dropProvider.innerRef}>
+                                {freeTasks.map((task, taskIndex) => (
+                                    <TaskCardComponent key={task.id} task={task} index={taskIndex} />
+                                ))}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
             </main>
             <MenuComponent showAddForm={() => setShowTaskForm(true)} />
             {showTaskForm && (
