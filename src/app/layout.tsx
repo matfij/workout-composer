99f1ee9dd564009335d14aff4ab6eca32cc48a7d
matfij <mateusz.michal.fijak@gmail.com>
2024-12-21 18:38:42 +0100

    Add open graph support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,26 @@ import { Suspense } from 'react';
 export const metadata: Metadata = {
     title: 'Workout Composer',
     manifest: 'manifest.json',
+    openGraph: {
+        title: 'Workout Composer',
+        type: 'website',
+        url: 'https://workout-composer.vercel.app',
+        images: {
+            href: 'https://workout-composer.vercel.app/',
+            hash: '',
+            host: '',
+            hostname: '',
+            origin: '',
+            password: '',
+            pathname: '',
+            port: '',
+            protocol: '',
+            search: '',
+            url: '',
+            username: '',
+            searchParams: new URLSearchParams(),
+        },
+    },
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
